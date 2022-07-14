1494f931d9e0b325b0915aa2b715113866021547
techner-niranjan <niranjan@technersolutions.com>
2022-07-14 12:38:43 +0530

    removed extra space

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
 const axios =  require("axios");
 
-
-    const url = 'http://api.weatherstack.com/current?access_key=125eba9badf202016ee32daf77565b73&query=37.8267,-122.4233&units=f'
-    const urlTwo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGVjaG5lci1uaXJhbmphbiIsImEiOiJjbDVrbDd3NDEwYXp5M2JwamplNXVubmhhIn0.z0v8NeQxf20ttnkL7uZw2w&limit=1'
+    const weatherstackApi = 'http://api.weatherstack.com/current?access_key=125eba9badf202016ee32daf77565b73&query=37.8267,-122.4233&units=f';
+    const mapboxApi = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGVjaG5lci1uaXJhbmphbiIsImEiOiJjbDVrbDd3NDEwYXp5M2JwamplNXVubmhhIn0.z0v8NeQxf20ttnkL7uZw2w&limit=1';
 
      axios
-    .get(url)
+    .get(weatherstackApi)
     .then((response) => {
         // console.log(response.data.current)
         let {temperature , feelslike, weather_descriptions } = response.data.current
-        console.log(`${weather_descriptions[0]}     and it is currently ${temperature} degree out . and feel like ${feelslike} degree`)
+        console.log(`${weather_descriptions[0]} and it is currently ${temperature} degree out . and feel like ${feelslike} degree`);
      })
-
     .catch((err)=>{
-        console.log(err)
-    })
-
+        console.log(err);
+    });
     axios
-    .get(urlTwo)
+    .get(mapboxApi)
     .then((response) => {
-        console.log(response.data)
-        
+        const[latitude , longitude] = response.data.features[0].center;
+        console.log(` latitude is ${latitude} and longitude is ${longitude}`);
      })
-
     .catch((err)=>{
-        console.log(err)
-    })
+        console.log(err);
+    });
     
 
 
@@ -40,19 +36,3 @@ const axios =  require("axios");
 
 
 
-
-
-
-
-// console.log('starting')
-
-// setTimeout(() => {
-//     console.log('2 second')
-// } , 2000)
-
-// setTimeout(() => {
-//     console.log('0 second')
-// } , 0)
-
-
-// console.log('stop')   
