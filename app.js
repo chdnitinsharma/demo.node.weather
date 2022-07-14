344db58bf63a8a661d9a961e5ebce42692045cb4
techner-niranjan <niranjan@technersolutions.com>
2022-07-14 11:41:23 +0530

    removed node modules

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const axios =  require("axios");
 
 
     const url = 'http://api.weatherstack.com/current?access_key=125eba9badf202016ee32daf77565b73&query=37.8267,-122.4233&units=f'
+    const urlTwo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGVjaG5lci1uaXJhbmphbiIsImEiOiJjbDVrbDd3NDEwYXp5M2JwamplNXVubmhhIn0.z0v8NeQxf20ttnkL7uZw2w&limit=1'
 
      axios
     .get(url)
@@ -15,7 +16,17 @@ const axios =  require("axios");
         console.log(err)
     })
 
-   
+    axios
+    .get(urlTwo)
+    .then((response) => {
+        console.log(response.data)
+        
+     })
+
+    .catch((err)=>{
+        console.log(err)
+    })
+    
 
 
 
