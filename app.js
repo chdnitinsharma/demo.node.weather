50f0d506185d3c41e63a6d815a8a649a7b2d3f38
techner-niranjan <niranjan@technersolutions.com>
2022-07-15 11:07:01 +0530

    use of process.argv

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const axios = require("axios");
 const geocode = require("./weather-data/geocode");
 const weatherstack = require("./weather-data/weatherstack");
 
-geocode("mohali", (error, data) => {
-  console.log("error", error);
-  console.log("data", data);
+const location = process.argv[2]
 
-  weatherstack( data.latitude , data.longitude, (error, data) => {
-    console.log("error", error);
-    console.log("data", data);
-  });
+if( !location ){
+  console.log('please provide a location');
+}else {
+  geocode(location, (error, data) => {
+    if(error){
+     return console.log(error)
+    };
   
-});
+    weatherstack( data.latitude , data.longitude, (error, weatherstackData) => {
+      if(error){
+        return console.log(error);
+      };
+      console.log(data.location);
+      console.log(weatherstackData);
+    });
+  });
+};
+
 
